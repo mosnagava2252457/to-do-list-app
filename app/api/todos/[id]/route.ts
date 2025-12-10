9b15c56bd822997fab30029d3f20bc6f2e3a5f7e
mosnagava2252457 <mosnagava.2252457@studenti.uniroma1.it>
2025-12-10 10:20:03 +0000

    fix: remove unused request parameter in DELETE handler

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -64,7 +64,7 @@ export async function PUT(request: Request, { params }: Params) {
   }
 }
 
-export async function DELETE(request: Request, { params }: Params) {
+export async function DELETE(_request: Request, { params }: Params) {
   try {
     const { error } = await supabaseAdmin
       .from('todos')
